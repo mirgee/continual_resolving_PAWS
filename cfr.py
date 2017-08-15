77cf703ad9f9be3e4f343e46bef77941356e8c5d
Miroslav Kovar <mrslvkvr@gmail.com>
2017-08-15 02:48:19 +0200

    Sigma maps from edges, not routes.

--- cfr.py
+++ cfr.py
@@ -1,17 +1,13 @@
 from itertools import product
-from random import sample
-import os
 import networkx as nx
-import numpy as np
 import pandas as pd
 
 max_depth = 4
 T = 10
 grid_dim_x = 1
 grid_dim_y = 2
-route_length = 40
 base = 0
-num_routes = 4
+route_length = 5
 
 def init():
 	df = pd.read_csv("/home/miroslav/Source/research_task/simple/nodes_list.txt", sep=" ")
@@ -23,16 +19,15 @@ def init():
 	global sigma1
 	sigma1 = [[(1/(grid_dim_x*grid_dim_y), 1/(grid_dim_x*grid_dim_y))] * grid_dim_y] * grid_dim_x
 	global average_strat1
-	accum_strat1 = [[(1/(grid_dim_x*grid_dim_y), 1/(grid_dim_x*grid_dim_y))] * grid_dim_y] * grid_dim_x
+	average_strat1 = [[(1/(grid_dim_x*grid_dim_y), 1/(grid_dim_x*grid_dim_y))] * grid_dim_y] * grid_dim_x
 	global regret1
 	regret1 = [[0] * grid_dim_y] * grid_dim_x
 	global sigma2
 	sigma2 = {} # Key: repr(routes), value: probability
 	global regret2
-	regret2 = {} # Key: (routes), value: regret
+	regret2 = {} # Key: repr(routes), value: regret
 	global average_strat2
 	average_strat2 = {}
-	return graph, dist
 
 def cfr_player1():
 	p1 = 1
@@ -42,8 +37,8 @@ def cfr_player1():
 		accum_val = 0
 
 		for (grid_x, grid_y) in product(range(grid_dim_x), range(grid_dim_y)):
-			cf_values1[grid_x][grid_y] = /
-				cfr_player2([], grid_x, grid_y, sigma1[grid_x][grid_y] * p1, p2,
+			cf_values1[grid_x][grid_y] = \
+				cfr_player2([(base,base)], grid_x, grid_y, sigma1[grid_x][grid_y] * p1, p2,
 				            route_length)
 			accum_val += sigma1[grid_x][grid_y] * cf_values1[grid_x][grid_y]
 
@@ -54,34 +49,34 @@ def cfr_player1():
 		regret_matching1()
 
 	average_strat1 = [average_strat1[i]/T for i in range(len(average_strat1))]
+	return average_strat1
 
 
 def cfr_player2(edge_history, grid_x, grid_y, p1, p2, rem_dist):
 	curr_node = edge_history[-1][1]
 
-	if rem_dist <= dist.iloc[curr_node, base] or curr_node == base:
+	if rem_dist <= dist.iloc[curr_node, base] or (len(edge_history) > 1 and curr_node == base):
 		route = route_from_edges(edge_history)
 		return compute_value_from_route(edge_history, grid_x, grid_y), route
 
 	edges = graph.edges(curr_node)
 	cf_values2 = [0] * len(edges)
-	regret2 = [0] * len(edges)
-	routes = [] * len(edges)
+	routes = [[]] * len(edges)
 	cf_value_curr = 0
 	for edge_index, edge in enumerate(edges):
 		edge_data = graph[edge[0]][edge[1]]
 		value, route = cfr_player2(edge_history.append(edge), grid_x, grid_y, p1,
-						edge_data['sigma'] * p2, rem_dist - edge_data['distance'])
+						sigma2[] * p2, rem_dist - edge_data['distance'])
 		cf_values2[edge_index] = value
 		# I = route_from_edges(edge_history) + route
 		cf_value_curr += sigma2[repr(route)] * value
 		routes.append(route)
 
 	for edge_index, edge in enumerate(edges):
-		regret2[edge_index] += p1 * (cf_values2[edge_index] - cf_value_curr)
+		regret2[routes[edge_index]] += p1 * (cf_values2[edge_index] - cf_value_curr)
 		average_strat2[repr(routes[edge_index])] += p2 * sigma2[repr(routes[edge_index])]
 
-	regret_matching2(curr_node)
+	regret_matching2(routes)
 
 	return cf_value_curr, edge_history
 
@@ -109,15 +104,17 @@ def regret_matching1():
 				sigma1[grid_x][grid_y] = 1/(grid_x*grid_y)
 
 
-def regret_matching2(curr_node):
-	edges = graph.edges(curr_node)
-	den = sum(edges.regret)
+def regret_matching2(routes):
+	regret = {}
+	for key, value in regret2:
+		regret[key] = max(value, 0)
+	den = sum(regret.values())
 	if den > 0:
-		for edge in edges:
-			edge.sigma = edge.regret / den
+		for route in routes:
+			sigma2[repr(route)] = regret[route] / den
 	else:
-		for edge in edges:
-			edge.sigma = 1/len(edges)
+		for route in routes:
+			sigma2[repr(route)] = 1/len(routes)
 
 
 def route_from_edges(edges):
@@ -130,4 +127,4 @@ def route_from_edges(edges):
 
 
 init()
-print(average_strat1)
+print(cfr_player1())
